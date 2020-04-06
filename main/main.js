8a2823e72f5aecfbbcbe51269e58e98380b5ddff
Veronica Sham <shamve@oocl.com>
2020-04-07 00:31:15 +0800

    commit

--- main/main.js
+++ main/main.js
@@ -2,14 +2,16 @@
 
 function printReceipt(inputs) {
     var boughtItemDetails = receiptProcessing(inputs); //array
-
-  console.log(countItem(gatherItemDetails(inputs)));
+    var Receipt = generateReceipt(calculateSingleItemPrice(countItem(gatherItemDetails(inputs))));
+//sumTotalPrice(calculateSingleItemPrice(countItem(gatherItemDetails(inputs))))
+  console.log(Receipt);
 }
 
 function receiptProcessing(itemIDList) { //input ARRAY
     var boughtItemDetails = [];
     var matchedIDItems = gatherItemDetails(itemIDList);
     var countedItem = countItem(matchedIDItems);
+    var singleItemPrice = calculateSingleItemPrice(countedItem); //object with quantity
     return boughtItemDetails; //array return to printReceipt
 }
 
@@ -26,32 +28,67 @@ function gatherItemDetails(itemIDList) {
 }
 
 function countItem(matchedIDItems) {
-    //var itemListWithQuantity = [];
     var itemListWithQuantity = loadAllItems();
+    itemListWithQuantity= itemListWithQuantity.map(function(o){o.quantity = 0; return o;});
     var loopMatchedItem;
-    for (loopMatchedItem = 0 ; loopMatchedItem< matchedIDItems.length ; loopMatchedItem ++) {
+    for (loopMatchedItem = 0 ; loopMatchedItem < matchedIDItems.length ; loopMatchedItem ++) {
         var quantity = matchedIDItems[loopMatchedItem].length;
-        // matchedIDItems[loopMatchedItem]['quantity'] = quantity;
-        //var temp = {"quantity": quantity};
-        itemListWithQuantity[loopMatchedItem]= itemListWithQuantity.map(function(o){o.quantity = quantity; return o;});
+        itemListWithQuantity[loopMatchedItem].quantity = quantity;
     }
-
-    //itemListWithQuantity= itemListWithQuantity.map(function(o){o.quantity = quantity; return o;});
     return itemListWithQuantity ; //array
 }
 
-function calculateSingleItemPrice(itemIDList) {
-    var itemQuantityAndPrice = [];
+function calculateSingleItemPrice(countedItem) {
+    var itemQuantityAndPrice = countedItem;
+    itemQuantityAndPrice= itemQuantityAndPrice.map(function(o){o.singlePrice = 0; return o;});
+    var loopCountedItem;
+    for (loopCountedItem = 0 ; loopCountedItem < countedItem.length ; loopCountedItem ++) {
+        var singlePrice = countedItem[loopCountedItem].price * countedItem[loopCountedItem].quantity;
+        itemQuantityAndPrice[loopCountedItem].singlePrice = singlePrice;
+    }
     return itemQuantityAndPrice; //array
+}
+
+function generateReceipt(itemQuantityAndPrice) {
+    var totalPrice = sumTotalPrice(itemQuantityAndPrice);
+    return presentReceipt(totalPrice, itemQuantityAndPrice);
+}
+
+function sumTotalPrice(itemQuantityAndPrice) {
+    var totalPrice = 0;
+    var loopAllItem;
+    for (loopAllItem = 0 ; loopAllItem < itemQuantityAndPrice.length ; loopAllItem ++){
+        totalPrice = totalPrice + parseFloat(itemQuantityAndPrice[loopAllItem].price) * parseInt(itemQuantityAndPrice[loopAllItem].quantity);
+    }
+    return totalPrice;
+}
+
+function presentReceipt(totalPrice, itemQuantityAndPrice) {
+    var itemString = "";
+    var loopAllItem;
+    for (loopAllItem = 0 ; loopAllItem < itemQuantityAndPrice.length ; loopAllItem ++){
+            if (itemQuantityAndPrice[loopAllItem].quantity != 0){
+            itemString = itemString +  "Name:"+ itemQuantityAndPrice[loopAllItem].name+"，Quantity:"+ itemQuantityAndPrice[loopAllItem].quantity+" "+
+            itemQuantityAndPrice[loopAllItem].unit;
+
+            if(itemQuantityAndPrice[loopAllItem].quantity > 1){
+                    itemString = itemString + "s,Unit:"+parseFloat(itemQuantityAndPrice[loopAllItem].price).toFixed(2)+" (yuan),Subtotal:"+
+                    parseFloat(itemQuantityAndPrice[loopAllItem].singlePrice).toFixed(2)+" (yuan)";
+            }
+
+            if(itemQuantityAndPrice[loopAllItem].quantity == 1){
+            itemString = itemString + ",Unit:"+parseFloat(itemQuantityAndPrice[loopAllItem].price).toFixed(2)+" (yuan),Subtotal:"+
+            parseFloat(itemQuantityAndPrice[loopAllItem].singlePrice).toFixed(2)+" (yuan)";
+            }
+
+                if(loopAllItem != itemQuantityAndPrice.length - 1){
+                    itemString = itemString + "\n";
+                }
+            }
+        }
+    return ("***<store earning no money>Receipt *** " + "\n" + itemString + "----------------------"+"\n"+
+            "total:"+ parseFloat(totalPrice).toFixed(2) +" (yuan)"+"\n"+"**********************");
 
 }
 
-function generateReceipt() {
 
-}
-
-function sumTotalPrice() {
-}
-
-function presentReceipt() {
-}
